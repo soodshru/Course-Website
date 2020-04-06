073f5eed3f275ca3305334033b074c7219a04d1b
soodshru <55465941+soodshru@users.noreply.github.com>
2020-04-06 14:04:27 -0400

    Implemented signup

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def check_data_student():
             accs = db.engine.execute(text(sql))
             for acc in accs:
                 if acc['UtorID'] == utorid and acc['Password'] == pw:
-                    session['user'] = utorid
+                    session['current_login'] = utorid
                     session['name'] = acc['Name']
                     return render_template('index_s.html')
             else:
@@ -78,7 +78,7 @@ def check_data_student():
 @app.route('/logout')
 def logout():
     # removes user from session
-    session.pop('user', None)
+    session.pop('current_login', None)
     session.pop('name', None)
     return redirect('/login.html')
 
@@ -87,90 +87,170 @@ def signup():
     # tells Flask to render the HTML page called signup.html
 	return render_template('signup.html')
 
+@app.route('/createaccform', methods=['GET','POST'])
+def createacc():
+    if request.method == 'POST':
+        type = str(request.form[UserType])
+        username = str(request.form[UserName])
+        loginid = str(request.form[LoginID])
+        pw = str(request.form[Password])
+        sql = """" INSERT INTO accounts VALUES ('{}', '{}', '{}', '{}') """.format(type,loginid,username,pw)
+        db.engine.execute(text(sql))
+        return render_template('login.html')
+
+
 @app.route('/announcements_i.html')
 def announcements_i():
-    # tells Flask to render the HTML page called /announcements_i.html
-	return render_template('/announcements_i.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /announcements_i.html
+	    return render_template('/announcements_i.html')
 
 @app.route('/announcements_s.html')
 def announcements_s():
-    # tells Flask to render the HTML page called /announcements_s.html
-	return render_template('/announcements_s.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /announcements_s.html
+	    return render_template('/announcements_s.html')
 
 @app.route('/evaluations_i.html')
 def evaluations_i():
-    # tells Flask to render the HTML page called /evaluations_i.html
-	return render_template('/evaluations_i.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /evaluations_i.html
+	    return render_template('/evaluations_i.html')
 
 @app.route('/evaluations_s.html')
 def evaluations_s():
-    # tells Flask to render the HTML page called /evaluations_s.html
-	return render_template('/evaluations_s.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /evaluations_s.html
+	    return render_template('/evaluations_s.html')
 
 @app.route('/index_i.html')
 def index_i():
-    # tells Flask to render the HTML page called /index_i.html
-	return render_template('/index_i.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /index_i.html
+	    return render_template('/index_i.html')
 
 @app.route('/index_s.html')
 def index_s():
-    # tells Flask to render the HTML page called /index_s.html
-	return render_template('/index_s.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /index_s.html
+	    return render_template('/index_s.html')
 
 @app.route('/labs_i.html')
 def labs_i():
-    # tells Flask to render the HTML page called /labs_i.html
-	return render_template('/labs_i.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /labs_i.html
+        return render_template('/labs_i.html')
 
 @app.route('/labs_s.html')
 def labs_s():
-    # tells Flask to render the HTML page called /labs_s.html
-	return render_template('/labs_s.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /labs_s.html
+        return render_template('/labs_s.html')
 
 @app.route('/lectures_i.html')
 def lectures_i():
-    # tells Flask to render the HTML page called /lectures_i.html
-	return render_template('/lectures_i.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /lectures_i.html
+        return render_template('/lectures_i.html')
 
 @app.route('/lectures_s.html')
 def lectures_s():
-    # tells Flask to render the HTML page called /lectures_s.html
-	return render_template('/lectures_s.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /lectures_s.html
+        return render_template('/lectures_s.html')
 
 @app.route('/feedback_i.html')
 def feedback_i():
-    # tells Flask to render the HTML page called /feedback_i.html
-	return render_template('/feedback_i.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /feedback_i.html
+        return render_template('/feedback_i.html')
 
 @app.route('/feedback_s.html')
 def feedback_s():
-    # tells Flask to render the HTML page called /feedback_s.html
-	return render_template('/feedback_s.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /feedback_s.html
+        return render_template('/feedback_s.html')
 
 @app.route('/resources_i.html')
 def resources_i():
-    # tells Flask to render the HTML page called /resources_i.html
-	return render_template('/resources_i.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /resources_i.html
+        return render_template('/resources_i.html')
 
 @app.route('/resources_s.html')
 def resources_s():
-    # tells Flask to render the HTML page called /resources_s.html
-	return render_template('/resources_s.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /resources_s.html
+        return render_template('/resources_s.html')
 
 @app.route('/grades_i.html')
 def grades_i():
-    # tells Flask to render the HTML page called /grades_i.html
-	return render_template('/grades_i.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /grades_i.html
+        return render_template('/grades_i.html')
 
 @app.route('/grades_s.html')
 def grades_s():
-    # tells Flask to render the HTML page called /grades_s.html
-	return render_template('/grades_s.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called /grades_s.html
+        return render_template('/grades_s.html')
 
 @app.route('/regrades_i.html')
 def regrades_i():
-    # tells Flask to render the HTML page called regrades_i.html
-	return render_template('regrades_i.html')
+    if session.get('current_login') == None:
+        # if user not logged in send them to the login page
+        return render_template('login.html')
+    else:
+        # tells Flask to render the HTML page called regrades_i.html
+        return render_template('regrades_i.html')
 
 if __name__ == '__main__':
 	app.run(debug ="true")
