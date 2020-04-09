3210e0f55787456f5e50f40c1864c592746a1d2f
Jeevikabaskaran <jeevika.baskaran@mail.utoronto.ca>
2020-04-09 18:58:42 -0400

    more js

--- static/app.js
+++ static/app.js
@@ -1,15 +1,15 @@
 function validate1() {
-	alert("You have signed up sucessfully");
+	return alert("You have signed up sucessfully");
 }
 
 function feedback_val1(){
 	return alert("You have succesfully submitted your feedback.");
 }
 
-function remark_val() {
+function remark_val1() {
 	return alert("You have successfully submitted a remark request.");
 }
 
-function grades_val(){
-	return alert('You have succesfully entered the grades.');
+function grades_val1(){
+	return alert("You have succesfully entered the grades.");
 }
--- templates/static/app.js
+++
@@ -1,8 +0,0 @@
-function validate1() {
-	alert("You have signed up sucessfully");
-}
-
-function feedback_val1() {
-	alert('You have successfully entered the regrade.');
-	return false;
-}
