8befa782d21b1a1235ef317a270f75afcd8d6b09
soodshru <55465941+soodshru@users.noreply.github.com>
2020-04-08 21:45:54 -0400

    minor changes

---
+++ static/app.js
@@ -0,0 +1,9 @@
+//$('signup').on('submit', function(){
+//	return window.confirm('You have successfully created an account.');
+//});
+
+
+
+function checkForm() {
+		return confirm('You have successfully created an account.');
+}
