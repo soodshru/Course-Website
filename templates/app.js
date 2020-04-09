9ce2db16b0d4169205bdfecc6a34d1ba5870d6ef
Jeevikabaskaran <jeevika.baskaran@mail.utoronto.ca>
2020-04-09 13:53:00 -0400

    js

--- templates/app.js
+++
@@ -1,7 +0,0 @@
-function validate() {
-	confirm ("You have signed up sucessfully")
-}
-
-function regrade_val() {
-	return confirm('You have successfully entered the regrade.');
-}
---
+++ templates/static/app.js
@@ -0,0 +1,8 @@
+function validate1() {
+	alert("You have signed up sucessfully");
+}
+
+function feedback_val() {
+	alert('You have successfully entered the regrade.');
+	return false;
+}
