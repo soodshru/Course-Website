f166a2ae9c52f85d30c4bde24c2d0b73d0d8f4bb
Jeevikabaskaran <jeevika.baskaran@mail.utoronto.ca>
2020-04-10 17:17:27 -0400

    report

---
+++ Assignment3/templates/static/Javascript/app2.js
@@ -0,0 +1,16 @@
+function validate1(){
+	return confirm("You have signed up successfully");
+}
+
+function feedback_val1(){
+	return confirm("Click Ok to submit your feedback.");
+}
+
+function remark_val1(){
+	return confirm("Click Ok to submit your remark request.");
+}
+
+
+function grades_val1(){
+	return confirm("Click Ok to enter the grades.");
+}
