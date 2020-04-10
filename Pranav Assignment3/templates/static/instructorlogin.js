0e3549f904ad861280b7ae09f8e12f424d14c120
soodshru <55465941+soodshru@users.noreply.github.com>
2020-04-10 19:37:44 -0400

    yo

--- Pranav Assignment3/templates/static/instructorlogin.js
+++
@@ -1,9 +0,0 @@
-function manage(txt) {
-        var bt = document.getElementById("btSubmit");
-        if (txt.value != '') {
-            bt.disabled = false;
-        }
-        else {
-            bt.disabled = true;
-        }
-    }    
--- Pranav Assignment3/templates/static/login3.js
+++
@@ -1,9 +0,0 @@
- function manage(txt) {
-        var bt = document.getElementById('submit');
-        if (txt.value != '') {
-            bt.disabled = false;
-        }
-        else {
-            bt.disabled = true;
-        }
-    }    
